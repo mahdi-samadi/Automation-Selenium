8a6db1379517440b00bc9a80d79f44f89ae0e66e
MahdiSamadi <mahdisamaadi@gmail.com>
2024-03-09 11:48:37 +0330

    finish part 3 and let's go to part 4

---
+++ part3.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get("https://wikipedia.com")
+
+# 1 => ID
+# el1 = driver.find_element('id', 'searchInput').send_keys('hello world!')
+
+# 2 => xpath
+# el2 = driver.find_element('xpath', '//input[@type="search"]').send_keys('hello world!')
+# print(el1)
+# print(el2)
+# assert el1 == el2
+# el3 = driver.find_element('xpath', '//*[text()="Italiano"]').click()
+
+# 3 ==> tag
+# el3 = driver.find_element('tag name', 'select').click()
+# sleep(4)
+
+# 4 ==> link text
+# el4 = driver.find_element('link text', 'Download Wikipedia for Android or iOS').click()
+# sleep(3)
+
+# 5 => partial link text
+# el5 = driver.find_element('partial link text', 'Download')
+
+#6 ==> class name
+# el6 = driver.find_element('class name', 'svg-search-icon').click()
+# sleep(3)
+
+#7 css selector
+# el7 = driver.find_element('css selector', '.svg-search-icon').click()
+# el8 = driver.find_element('css selector', '#searchInput').click()
