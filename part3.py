1eff5a59eafb655a469ef5cf13468e2e0bad8d25
mahdi-samadi <mahdisamaadi@gmail.com>
2024-03-10 19:02:21 +0330

    add part4

--- part3.py
+++ part3.py
@@ -5,32 +5,32 @@ driver = webdriver.Chrome()
 driver.get("https://wikipedia.com")
 
 # 1 => ID
-# el1 = driver.find_element('id', 'searchInput').send_keys('hello world!')
+el1 = driver.find_element('id', 'searchInput').send_keys('hello world!')
 
 # 2 => xpath
-# el2 = driver.find_element('xpath', '//input[@type="search"]').send_keys('hello world!')
-# print(el1)
-# print(el2)
-# assert el1 == el2
-# el3 = driver.find_element('xpath', '//*[text()="Italiano"]').click()
+el2 = driver.find_element('xpath', '//input[@type="search"]').send_keys('hello world!')
+print(el1)
+print(el2)
+assert el1 == el2
+el3 = driver.find_element('xpath', '//*[text()="Italiano"]').click()
 
 # 3 ==> tag
-# el3 = driver.find_element('tag name', 'select').click()
-# sleep(4)
+el3 = driver.find_element('tag name', 'select').click()
+sleep(4)
 
 # 4 ==> link text
-# el4 = driver.find_element('link text', 'Download Wikipedia for Android or iOS').click()
-# sleep(3)
+el4 = driver.find_element('link text', 'Download Wikipedia for Android or iOS').click()
+sleep(3)
 
 # 5 => partial link text
-# el5 = driver.find_element('partial link text', 'Download')
+el5 = driver.find_element('partial link text', 'Download')
 
 #6 ==> class name
-# el6 = driver.find_element('class name', 'svg-search-icon').click()
-# sleep(3)
+el6 = driver.find_element('class name', 'svg-search-icon').click()
+sleep(3)
 
 #7 css selector
-# el7 = driver.find_element('css selector', '.svg-search-icon').click()
-# el8 = driver.find_element('css selector', '#searchInput').click()
-# el8 = driver.find_element('css selector', 'body.jsl10n-visible:nth-child(2) main:nth-child(1) div.search-container:nth-child(3) form.pure-form fieldset:nth-child(1) > button.pure-button.pure-button-primary-progressive:nth-child(4)').click()
-# sleep(3)
+el7 = driver.find_element('css selector', '.svg-search-icon').click()
+el8 = driver.find_element('css selector', '#searchInput').click()
+el8 = driver.find_element('css selector', 'body.jsl10n-visible:nth-child(2) main:nth-child(1) div.search-container:nth-child(3) form.pure-form fieldset:nth-child(1) > button.pure-button.pure-button-primary-progressive:nth-child(4)').click()
+sleep(3)
