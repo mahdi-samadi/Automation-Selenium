ba09608fa9be09a9ce87d8d8e95c81fcab8c253b
mahdi-samadi <mahdisamaadi@gmail.com>
2024-04-20 16:31:39 +0330

    final part episode 7

--- tests/loginTests.py
+++ tests/loginTests.py
@@ -6,17 +6,25 @@ from pages.mainPage import mainPage
 import unittest
 
 
-class LoginTests(unittest.TestCase)
-driver = webdriver.Chrome()
-driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-driver.implicitly_wait(3) # yani ta 3 saniye vasta ta amaliyat anjam beshe age 1 saniye tol keshid dg ta 3 montazer nabash
+class LoginTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(3) # yani ta 3 saniye vasta ta amaliyat anjam beshe age 1 saniye tol keshid dg ta 3 montazer nabash
+        cls.driver.maximize_window()
 
-login = login(driver = driver)
-main_page = mainPage(driver = driver)
+    def test_valid_login(self):
+        self.driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        login = login(driver = self.driver)
+        main_page = mainPage(driver = self.driver)
+        login.enter_username('Admin')
+        login.enter_password('admin123')
+        login.click_on_login_button()
+        main_page.check_main_page()
+        sleep(2)
 
-login.enter_username('Admin')
-login.enter_password('admin123')
-login.click_on_login_button()
-main_page.check_main_page()
-sleep(2)
+    @classmethod
+    def tearDownClass(self) -> None:
+        self.driver.close()
+        self.driver.quit()
