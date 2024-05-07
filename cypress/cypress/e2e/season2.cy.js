6599f9d1d382e6c21ddf113e700e10429baa6d1d
mahdi-samadi <mahdisamaadi@gmail.com>
2024-05-08 00:48:46 +0330

    Start part2

---
+++ cypress/cypress/e2e/season2.cy.js
@@ -0,0 +1,7 @@
+/// <reference types = "cypress"/>
+describe("season2", () => {
+  it("test1", () => {
+    cy.visit("https://play2.automationcamp.ir/");
+    // cy.get("#fname").type("mahdi");
+  });
+});
